275bd690f3bcaf568378039c3c6c4599356bb4fc
Johan Norr <johan.norr84@gmail.com>
2024-01-24 13:41:48 +0100

    Updated header

--- src/components/ShowHeader.tsx
+++ src/components/ShowHeader.tsx
@@ -10,36 +10,18 @@ function ShowHeader() {
               href="/"
               className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
             >
-              <svg className="bi me-2" width="40" height="32">
-                <use xlink:href="#bootstrap"></use>
-              </svg>
-              <span className="fs-4">Simple header</span>
+              <span className="fs-4">Todo list for Jonor84</span>
             </a>
 
             <ul className="nav nav-pills">
               <li className="nav-item">
                 <a href="#" className="nav-link active" aria-current="page">
-                  Home
+                  Show list
                 </a>
               </li>
               <li className="nav-item">
                 <a href="#" className="nav-link">
-                  Features
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  FAQs
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#" className="nav-link">
-                  About
+                  Add item
                 </a>
               </li>
             </ul>
