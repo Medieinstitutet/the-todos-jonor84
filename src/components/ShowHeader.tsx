9f5d57bb011b7e0e50909a95fb66c6d303ab1a58
Johan Norr <johan.norr84@gmail.com>
2024-01-24 15:27:55 +0100

    Changed Link to NavLink to use active page visable

--- src/components/ShowHeader.tsx
+++ src/components/ShowHeader.tsx
@@ -1,31 +1,31 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function ShowHeader() {
   return (
     <div className="container">
       <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom sticky-header">
-        <Link
+        <NavLink
           to="/"
           className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
         >
           <span className="fs-4">Todo list for Jonor84</span>
-        </Link>
+        </NavLink>
 
         <ul className="nav nav-pills">
           <li className="nav-item">
-            <Link
+            <NavLink
               to="/"
               className="nav-link"
               activeclassname="active"
               aria-current="page"
             >
               Show list
-            </Link>
+            </NavLink>
           </li>
           <li className="nav-item">
-            <Link to="/add" className="nav-link">
+            <NavLink to="/add" className="nav-link">
               Add item
-            </Link>
+            </NavLink>
           </li>
         </ul>
       </header>
